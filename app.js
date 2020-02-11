761e1baf64f89ee0273cabd86681e73aa44cdb3c
Javi1499 <fruiz1@ucol.mx>
2020-02-11 07:17:48 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const superhero = require('superhero-name-library')
+
+console.log(superhero.random());
+
+var names = require('simpsons-names');
+var allNames = names.all;
+var randomName = names.random();
+var threeRandomNames = names.random(3);
+
+console.log(names.random());
